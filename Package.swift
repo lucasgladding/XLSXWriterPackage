a3a1056cc9cbe0ab9bb24a43862faba9867b7e54
Lucas Gladding <lucas@me.com>
2023-06-07 12:53:49 -0400

    Added dependency.

--- Package.swift
+++ Package.swift
@@ -8,6 +8,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
+            name: "xlsxwriter",
+            targets: ["xlsxwriter"]),
+        .library(
             name: "XLSXWriterPackage",
             targets: ["XLSXWriterPackage"]),
     ],
@@ -18,6 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .systemLibrary(name: "Cxlsxwriter", pkgConfig: "xlsxwriter"),
         .target(
             name: "XLSXWriterPackage",
             dependencies: []),

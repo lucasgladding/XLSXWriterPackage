12f30c130dbe8fdbf345a514025befb6f53a4d67
Lucas Gladding <lucas@me.com>
2023-06-11 01:14:53 -0400

    Refactor.

--- Sources/XLSXWriterPackage/Format.swift
+++ Sources/XLSXWriterPackage/Format.swift
@@ -9,13 +9,46 @@ import Foundation
 import libxlsxwriter
 
 public class Format {
+    public enum Options {
+        case alignment(Alignment)
+        case backgroundColor(Color)
+        case bold
+        case border(Border)
+        case fontColor(Color)
+        case fontScript(Script)
+        case italic
+        case number(String)
+        case underline(Underline)
+    }
+
     public enum Alignment {
         case center
         case verticalCenter
+        
+        var lxwAlignment: UInt8? {
+            let options: [Alignment: lxw_format_alignments] = [
+                .center: LXW_ALIGN_CENTER,
+                .verticalCenter: LXW_ALIGN_VERTICAL_CENTER
+            ]
+            guard let option = options[self] else {
+                return nil
+            }
+            return UInt8(option.rawValue)
+        }
     }
 
     public enum Border {
         case thin
+        
+        var lxwBorder: UInt8? {
+            let options: [Border: lxw_format_borders] = [
+                .thin: LXW_BORDER_THIN
+            ]
+            guard let option = options[self] else {
+                return nil
+            }
+            return UInt8(option.rawValue)
+        }
     }
 
     public enum Color {
@@ -34,27 +67,60 @@ public class Format {
         case silver
         case white
         case yellow
+
+        var lxwColor: UInt32? {
+            let options: [Color: lxw_defined_colors] = [
+                .black: LXW_COLOR_BLACK,
+                .blue: LXW_COLOR_BLUE,
+                .cyan: LXW_COLOR_CYAN,
+                .gray: LXW_COLOR_GRAY,
+                .green: LXW_COLOR_GREEN,
+                .lime: LXW_COLOR_LIME,
+                .magenta: LXW_COLOR_MAGENTA,
+                .navy: LXW_COLOR_NAVY,
+                .orange: LXW_COLOR_ORANGE,
+                .pink: LXW_COLOR_PINK,
+                .purple: LXW_COLOR_PURPLE,
+                .red: LXW_COLOR_RED,
+                .silver: LXW_COLOR_SILVER,
+                .white: LXW_COLOR_WHITE,
+                .yellow: LXW_COLOR_YELLOW
+            ]
+            guard let option = options[self] else {
+                return nil
+            }
+            return UInt32(option.rawValue)
+        }
     }
 
     public enum Script {
         case superscript
+        
+        var lxwScript: UInt8? {
+            let options: [Script: lxw_format_scripts] = [
+                .superscript: LXW_FONT_SUPERSCRIPT
+            ]
+            guard let option = options[self] else {
+                return nil
+            }
+            return UInt8(option.rawValue)
+        }
     }
 
     public enum Underline {
         case double
         case single
-    }
-
-    public enum Options {
-        case alignment(Alignment)
-        case backgroundColor(Color)
-        case bold
-        case border(Border)
-        case fontColor(Color)
-        case fontScript(Script)
-        case italic
-        case number(String)
-        case underline(Underline)
+        
+        var lxwUnderline: UInt8? {
+            let options: [Underline: lxw_format_underlines] = [
+                .double: LXW_UNDERLINE_DOUBLE,
+                .single: LXW_UNDERLINE_SINGLE
+            ]
+            guard let option = options[self] else {
+                return nil
+            }
+            return UInt8(option.rawValue)
+        }
     }
 
     let lxw_format: UnsafeMutablePointer<lxw_format>
@@ -66,101 +132,35 @@ public class Format {
     public func set(_ options: Options) {
         switch options {
         case .alignment(let input):
-            if let alignment = get_lxw_alignment(from: input) {
-                format_set_align(self.lxw_format, alignment)
+            if let option = input.lxwAlignment {
+                format_set_align(self.lxw_format, option)
             }
         case .backgroundColor(let input):
-            if let backgroundColor = get_lxw_color(from: input) {
-                format_set_bg_color(self.lxw_format, backgroundColor)
+            if let option = input.lxwColor {
+                format_set_bg_color(self.lxw_format, option)
             }
         case .border(let input):
-            if let border = get_lxw_border(from: input) {
-                format_set_border(self.lxw_format, border)
+            if let option = input.lxwBorder {
+                format_set_border(self.lxw_format, option)
             }
         case .bold:
             format_set_bold(self.lxw_format)
         case .fontColor(let input):
-            if let fontColor = get_lxw_color(from: input) {
-                format_set_font_color(self.lxw_format, fontColor)
+            if let option = input.lxwColor {
+                format_set_font_color(self.lxw_format, option)
             }
         case .fontScript(let input):
-            if let fontScript = get_lxw_script(from: input) {
-                format_set_font_script(self.lxw_format, fontScript)
+            if let option = input.lxwScript {
+                format_set_font_script(self.lxw_format, option)
             }
         case .italic:
             format_set_italic(self.lxw_format)
         case .number(let input):
-            input.withCString { format_set_num_format(self.lxw_format, $0) }
+            format_set_num_format(self.lxw_format, input.cString)
         case .underline(let input):
-            if let underline = get_lxw_underline(from: input) {
-                format_set_underline(self.lxw_format, underline)
+            if let option = input.lxwUnderline {
+                format_set_underline(self.lxw_format, option)
             }
         }
-    }
-
-    private func get_lxw_alignment(from alignment: Alignment) -> UInt8? {
-        let styles: [Alignment: lxw_format_alignments] = [
-            .center: LXW_ALIGN_CENTER,
-            .verticalCenter: LXW_ALIGN_VERTICAL_CENTER
-        ]
-        guard let style = styles[alignment] else {
-            return nil
-        }
-        return UInt8(style.rawValue)
-    }
-
-    private func get_lxw_border(from border: Border) -> UInt8? {
-        let styles: [Border: lxw_format_borders] = [
-            .thin: LXW_BORDER_THIN
-        ]
-        guard let style = styles[border] else {
-            return nil
-        }
-        return UInt8(style.rawValue)
-    }
-
-    private func get_lxw_color(from color: Color) -> UInt32? {
-        let styles: [Color: lxw_defined_colors] = [
-            .black: LXW_COLOR_BLACK,
-            .blue: LXW_COLOR_BLUE,
-            .cyan: LXW_COLOR_CYAN,
-            .gray: LXW_COLOR_GRAY,
-            .green: LXW_COLOR_GREEN,
-            .lime: LXW_COLOR_LIME,
-            .magenta: LXW_COLOR_MAGENTA,
-            .navy: LXW_COLOR_NAVY,
-            .orange: LXW_COLOR_ORANGE,
-            .pink: LXW_COLOR_PINK,
-            .purple: LXW_COLOR_PURPLE,
-            .red: LXW_COLOR_RED,
-            .silver: LXW_COLOR_SILVER,
-            .white: LXW_COLOR_WHITE,
-            .yellow: LXW_COLOR_YELLOW
-        ]
-        guard let style = styles[color] else {
-            return nil
-        }
-        return UInt32(style.rawValue)
-    }
-
-    private func get_lxw_script(from script: Script) -> UInt8? {
-        let styles: [Script: lxw_format_scripts] = [
-            .superscript: LXW_FONT_SUPERSCRIPT
-        ]
-        guard let style = styles[script] else {
-            return nil
-        }
-        return UInt8(style.rawValue)
-    }
-
-    private func get_lxw_underline(from underline: Underline) -> UInt8? {
-        let styles: [Underline: lxw_format_underlines] = [
-            .double: LXW_UNDERLINE_DOUBLE,
-            .single: LXW_UNDERLINE_SINGLE
-        ]
-        guard let style = styles[underline] else {
-            return nil
-        }
-        return UInt8(style.rawValue)
     }
 }

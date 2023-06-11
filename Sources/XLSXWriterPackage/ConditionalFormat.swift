86aaed7d57b1534f351dfa91ddcf74b605f9c1fe
Lucas Gladding <lucas@me.com>
2023-06-11 09:44:07 -0400

    Refactor.

--- Sources/XLSXWriterPackage/ConditionalFormat.swift
+++ Sources/XLSXWriterPackage/ConditionalFormat.swift
@@ -12,10 +12,31 @@ public struct ConditionalFormat {
     public enum ConditionalFormatType {
         case none
         case cell
+
+        var lxwConditionalFormatType: UInt8? {
+            let options: [ConditionalFormatType: lxw_conditional_format_types] = [
+                .none: LXW_CONDITIONAL_TYPE_NONE,
+                .cell: LXW_CONDITIONAL_TYPE_CELL
+            ]
+            guard let option = options[self] else {
+                return nil
+            }
+            return UInt8(option.rawValue)
+        }
     }
 
     public enum ConditionalFormatCriteria {
         case lessThan
+
+        var lxwConditionalFormatCriteria: UInt8? {
+            let options: [ConditionalFormatCriteria: lxw_conditional_criteria] = [
+                .lessThan: LXW_CONDITIONAL_CRITERIA_LESS_THAN
+            ]
+            guard let option = options[self] else {
+                return nil
+            }
+            return UInt8(option.rawValue)
+        }
     }
 
     let type: ConditionalFormatType
@@ -32,32 +53,11 @@ public struct ConditionalFormat {
 
     var lxwConditionalFormat: lxw_conditional_format {
         var format = lxw_conditional_format()
-        format.type = lxwConditionalFormatType!
-        format.criteria = lxwConditionalFormatCriteria!
+        format.type = self.type.lxwConditionalFormatType!
+        format.criteria = self.criteria.lxwConditionalFormatCriteria!
         format.value = self.value
         format.format = self.format.lxw_format
 
         return format
-    }
-
-    var lxwConditionalFormatType: UInt8? {
-        let options: [ConditionalFormatType: lxw_conditional_format_types] = [
-            .none: LXW_CONDITIONAL_TYPE_NONE,
-            .cell: LXW_CONDITIONAL_TYPE_CELL
-        ]
-        guard let option = options[self.type] else {
-            return nil
-        }
-        return UInt8(option.rawValue)
-    }
-
-    var lxwConditionalFormatCriteria: UInt8? {
-        let options: [ConditionalFormatCriteria: lxw_conditional_criteria] = [
-            .lessThan: LXW_CONDITIONAL_CRITERIA_LESS_THAN
-        ]
-        guard let option = options[self.criteria] else {
-            return nil
-        }
-        return UInt8(option.rawValue)
     }
 }
